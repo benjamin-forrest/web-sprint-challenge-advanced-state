c435318c5372db5b2b935ff6df5433d4971e3250
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-05-04 14:59:55 -0700

    initial form state and form reducers and functions

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -53,7 +53,14 @@ const initialFormState = {
   newFalseAnswer: '',
 }
 function form(state = initialFormState, action) {
-  return state
+  switch(action.type){
+    case INPUT_CHANGE:
+      return {...state, [action.payload.id]: action.payload.value}
+    case RESET_FORM:
+      return initialFormState
+    default:
+      return state
+  }
 }
 
 export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
