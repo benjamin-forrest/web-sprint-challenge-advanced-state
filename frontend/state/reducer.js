ad3633f0aadc56cefdd5d1f51b65c0249be3a635
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-05-04 14:53:52 -0700

    initial message state reducer

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -29,12 +29,22 @@ function quiz(state = initialQuizState, action) {
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch(action.type) {
+    case SET_SELECTED_ANSWER:
+      return action.payload
+      default:
+        return state
+  }
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch(action.type) {
+    case SET_INFO_MESSAGE:
+      return action.payload
+      default:
+        return state
+  }
 }
 
 const initialFormState = {
